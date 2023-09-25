c4d7f2ce2d82cbff53b4080ef5db88cb63e37b02
Renato Paracone <renato.paracone@hotmail.com>
2023-09-25 15:42:31 -0300

    Actualizaciones

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+import { Home } from "./components/pages/home/home";
+import { Login } from "./components/pages/login/login";
+
+function App() {
+
+  return (
+    <h1>Hola</h1>
+  )
+}
+
+export default App
